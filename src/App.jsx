4a2cc4fe98be3e7e1ae1c0647800e3009d7fb236
LEAKONO <leakonoemmanuel3@gmail.com>
2026-02-21 12:38:44 +0300

    Add user search functionality with filtered list

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import UserList from './components/UserList'
+import SearchBar from './components/SearchBar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[items,setItems]=useState([])
+  const[search,setSearch]=useState("")
 
+  useEffect(()=>{
+          fetch("https://jsonplaceholder.typicode.com/users")
+          .then(res=>res.json())
+          .then(data=>setItems(data))
+          .catch(err=>console.error(err))
+    },[])
+  const filteredData = items.filter(item =>
+  item.name.toLowerCase().includes(search.toLowerCase())
+)
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+     <SearchBar search={search} setSearch={setSearch}/>
+      <UserList  filtered={filteredData}/>
+    </div>
   )
 }
 
