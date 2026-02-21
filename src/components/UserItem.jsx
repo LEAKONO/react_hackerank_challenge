1cdb6f4b0975214dd5f240ab0305c814680c7aad
LEAKONO <leakonoemmanuel3@gmail.com>
2026-02-21 13:03:17 +0300

    Fix UserItem to display nested address and company info

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -1,8 +1,19 @@
+
+
 import React from 'react'
 
-function UserItem() {
+function UserItem({ user }) {
   return (
-    <div>UserItem</div>
+    <div style={{border: "1px solid gray", padding: "10px", margin: "10px 0"}}>
+      <h2>{user.name}</h2>
+      <h4>@{user.username}</h4>
+      <p>Email: {user.email}</p>
+      <p>
+        Address: {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}
+      </p>
+      <p>Website: {user.website}</p>
+      <p>Company: {user.company.name} - {user.company.catchPhrase}</p>
+    </div>
   )
 }
 
